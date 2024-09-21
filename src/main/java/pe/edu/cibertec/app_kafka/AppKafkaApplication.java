c10eb6864036d3cd5344aa803fe999e9c4ab7349
ProgramandoWeb2 <daplicacionesmoviles1@gmail.com>
2024-09-21 16:43:02 -0500

    proyecto inicial

---
+++ src/main/java/pe/edu/cibertec/app_kafka/AppKafkaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_kafka/AppKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
